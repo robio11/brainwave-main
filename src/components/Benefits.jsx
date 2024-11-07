5e40955178c04404e5bce788f7d5f5a7ed6eb5d2
Rachana Meena <249rachanameena@gmail.com>
2024-11-07 14:09:39 +0530

    main

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -13,7 +13,6 @@ const Benefits = () => {
           className="md:max-w-md lg:max-w-2xl"
           title="Chat Smarter, Not Harder with Brainwave"
         />
-
         <div className="flex flex-wrap gap-10 mb-10">
           {benefits.map((item) => (
             <div
